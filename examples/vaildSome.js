67d073e1d9ac51eba5da74644fab8a614bb08906
miniflycn <miniflycn@justany.net>
2013-09-27 16:17:41 +0800

    support https
    support https

--- examples/vaildSome.js
+++ examples/vaildSome.js
@@ -5,7 +5,12 @@ module.exports = (function () {
   vaild.some([
   	'http://www.baidu.com',
   	'http://www.google.com',
-  	'http://www.nothissite.com'
+  	'http://www.nothissite.com',
+    'http://www.cnblogs.com',
+    'http://www.sohu.com',
+    'http://www.sina.com',
+    'http://www.yeeyan.org',
+    'http://github.com'
   ], function (err, data) {
   	err && console.log(err);
   	console.log(data);
