4eb50d248fee659360065b5df53d910003a7a5bb
miniflycn <miniflycn@justany.net>
2013-09-27 23:45:42 +0800

    refactor & support on event
    refactor & support on event

--- examples/vaildSome.js
+++ examples/vaildSome.js
@@ -3,16 +3,16 @@ module.exports = (function () {
   var vaild = require('../');
 
   vaild.some([
-  	'http://www.baidu.com',
-  	'http://www.google.com',
-  	'http://www.nothissite.com',
+    'http://www.baidu.com',
+    'http://www.google.com',
+    'http://www.nothissite.com',
     'http://www.cnblogs.com',
     'http://www.sohu.com',
     'http://www.sina.com',
     'http://www.yeeyan.org',
     'http://github.com'
   ], function (err, data) {
-  	err && console.log(err);
-  	console.log(data);
+    err && console.log(err);
+    console.log(data);
   });
 })();
