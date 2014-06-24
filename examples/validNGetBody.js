5705f3f75fc88686c5bda9289a29563de182ca84
Daniel Yang <miniflycn@justany.net>
2014-06-24 23:33:13 +0800

    remove .one examples

--- examples/validNGetBody.js
+++ examples/validNGetBody.js
@@ -2,7 +2,7 @@ module.exports = (function () {
   "use strict"
   var valid = require('../');
 
-  valid.one('http://www.google.com', function (err, valid) {
+  valid('http://www.google.com', function (err, valid) {
   	if (err) throw err;
   	console.log(valid);
   }).on('data', function (err, data) {
